1ef6247eee838c19994c93fd598efba8ce272686
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-03-31 19:30:39 +0100

    working

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css"; // Global styles
+import ExtensionsList from "./ExtensionsList.Jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <ExtensionsList />
+    </div>
+  );
+}
+
+export default App;

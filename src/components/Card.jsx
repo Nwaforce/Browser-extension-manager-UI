4deeeea026949cf4da59e35d39e64fe28ab54344
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-04-07 19:23:02 +0100

    still

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,20 +4,25 @@ import "../styles.css";
 const Card = ({ extension, onToggle, onRemove }) => {
   return (
     <div className={`card ${extension.isActive ? "active" : "inactive"}`}>
-       <div className="card-content">
-       <img src={extension.logo} alt={extension.name} className="card-logo" />
+      <div className="card-content">
+        <img src={extension.logo} alt={extension.name} className="card-logo" />
         <div>
-        <h2>{extension.name}</h2>
-        <p>{extension.description}</p>
+          <h2>{extension.name}</h2>
+          <p>{extension.description}</p>
         </div>
-        </div>
-        <div className="card-actions">
-          <button className="remove-btn" onClick={onRemove}>Remove</button>
-          <label className="switch">
-            <input type="checkbox" checked={extension.isActive} onChange={onToggle} />
-            <span className="slider"></span>
-          </label>
-        
+      </div>
+      <div className="card-actions">
+        <button className="remove-btn" onClick={onRemove}>
+          Remove
+        </button>
+        <label className="switch">
+          <input
+            type="checkbox"
+            checked={extension.isActive}
+            onChange={onToggle}
+          />
+          <span className="slider"></span>
+        </label>
       </div>
     </div>
   );
